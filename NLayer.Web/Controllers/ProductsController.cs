9949d2c88821f302c51c11d0ce2485a88cb47503
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2024-07-01 18:49:51 +0300

    Product Save method created

--- NLayer.Web/Controllers/ProductsController.cs
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
 using NLayer.Core.Services;
 
 namespace NLayer.Web.Controllers
@@ -6,10 +10,14 @@ namespace NLayer.Web.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductService _services;
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
 
-        public ProductsController(IProductService services)
+        public ProductsController(IProductService services, ICategoryService categoryService, IMapper mapper)
         {
             _services = services;
+            _categoryService = categoryService;
+            _mapper = mapper;
         }
 
         public async Task<IActionResult> Index()
@@ -18,6 +26,32 @@ namespace NLayer.Web.Controllers
             //sadece dataları aldık return içinde
             var customeResponse = await _services.GetProductsWithCategory();
             return View(customeResponse.Data);
+        }
+
+        public async Task<IActionResult> Save()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+
+            //ViewBag ile bir liste aç, bu liste dtodan gelcek, tıklandıgında id yi alcak, kullanıcılar name görcek
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+            if(ModelState.IsValid)
+            {
+                await _services.AddAsync(_mapper.Map<Product>(productDto));
+                return RedirectToAction(nameof(Index));
+            }
+
+            var categories = await _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name");
+            return View();
         }
     }
 }
--- NLayer.Web/obj/Debug/net6.0/NLayer.Web.AssemblyInfo.cs
+++ NLayer.Web/obj/Debug/net6.0/NLayer.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NLayer.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f21bd7c2eb614f9f79a835180dfcf00fb965818f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb500f99e802935380fd4cbebf19f5a9b0f29695")]
 [assembly: System.Reflection.AssemblyProductAttribute("NLayer.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NLayer.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
